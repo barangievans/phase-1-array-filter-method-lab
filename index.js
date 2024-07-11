13ccd65d1954ec038383332b38e5e077f4c00309
barangievans <barangievans@gmail.com>
2024-07-11 15:17:58 +0300

    filter-method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+// Function: findMatching
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+}
+
+// Function: fuzzyMatch
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+// Function: matchName
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
+
+module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+};
+
+
